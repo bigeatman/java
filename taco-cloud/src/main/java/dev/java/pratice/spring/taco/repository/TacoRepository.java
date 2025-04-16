6314ada11dd75658c1d24de6838a2bc5094dc9de
ProcoKim <dudgp2030@gmail.com>
2025-04-16 19:17:45 +0900

    spring ch3 - JPA

--- taco-cloud/src/main/java/dev/java/pratice/spring/taco/repository/TacoRepository.java
+++ taco-cloud/src/main/java/dev/java/pratice/spring/taco/repository/TacoRepository.java
@@ -1,9 +1,9 @@
 package dev.java.pratice.spring.taco.repository;
 
+import org.springframework.data.repository.CrudRepository;
+
 import dev.java.pratice.spring.taco.model.Taco;
 
-public interface TacoRepository {
-
-	Taco save(Taco design);
+public interface TacoRepository extends CrudRepository<Taco, Long> {
 
 }
